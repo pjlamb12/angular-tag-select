daa1cd21bf9a0bd0531a58077b3f169dc4b4faa6
Preston Lamb <preston.j.lamb@gmail.com>
2017-11-08 22:23:15 -0700

    Added tag filter pipe

--- src/app/tag-select/tag-select.module.ts
+++ src/app/tag-select/tag-select.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagSelectComponent } from './tag-select/tag-select.component';
+import { TagFilterPipe } from './tag-filter.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TagSelectComponent],
+  declarations: [TagSelectComponent, TagFilterPipe],
   exports: [TagSelectComponent]
 })
 export class TagSelectModule { }
