adc2ed4ac34c4f6fa1b4b9768448755cc2961c4a
Preston Lamb <preston.j.lamb@gmail.com>
2018-07-15 14:59:56 -0600

    Added needed modules, and template content

--- projects/angular-tag-select-lib/src/lib/angular-tag-select-lib.module.ts
+++ projects/angular-tag-select-lib/src/lib/angular-tag-select-lib.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { TagSelectComponent } from './tag-select/tag-select.component';
 import { TagFilterPipe } from './tag-filter.pipe';
 
 @NgModule({
-	imports: [],
+	imports: [CommonModule, FormsModule],
 	declarations: [TagSelectComponent, TagFilterPipe],
 	exports: [TagSelectComponent],
 })
